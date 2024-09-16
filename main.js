216e4813c89d2b9c5e94be05ff32ab240136fb39
Noah Van Fleet <noahvanfleet@gmail.com>
2024-09-16 14:33:14 -0400

    Temp fixes for job fair

--- main.js
+++ main.js
@@ -76,7 +76,7 @@ function populateSherlockModal() {
     description.innerHTML = "Sherlock is a cross platform mobile app built to track local crime, sexual offenders, and dangerous locations by the hour in a map view for Knox county, TN. ";
 
     let tech = document.querySelector(".modal-tech");
-    tech.innerHTML = "Sherlock was crafted utilizing React Native for frontend development, Node.js for backend functionality, SQLite3 for robust database management, and AWS Cognito for secure user authentication and verification.";
+    tech.innerHTML = "Sherlock was crafted utilizing React Native for a simple cross platform development experience. As well as utilizing the Google Maps API for a uniform mapping experience across all devices.";
 
     let goals = document.getElementById("modal-goals");
     goals.children[0].innerHTML = "Show all locally reported crime in a map view."
@@ -85,10 +85,10 @@ function populateSherlockModal() {
     goals.children[3].innerHTML = "Users can post anonymously with randomly assigned crime based display names.";
 
     let challenges = document.getElementById("modal-challenges");
-    challenges.children[0].innerHTML = "Sherlock challenge 1";
-    challenges.children[1].children[0].innerHTML = "Sherlock challenge 1 more info";
-    challenges.children[2].innerHTML = "Sherlock challenge 2";
-    challenges.children[3].innerHTML = "Sherlock challenge 3";
+    challenges.children[0].innerHTML = "Unfamiliarity with React Native and Google Maps API.";
+    // challenges.children[1].children[0].innerHTML = "Sherlock challenge 1 more info";
+    challenges.children[2].innerHTML = "Race conditions with filtering and displaying crime data.";
+    challenges.children[3].innerHTML = "App store requirements and legalities.";
 
 }
 
@@ -109,25 +109,25 @@ let proj2TitleOpen = document.getElementById("proj2-title");
 
 function populateProj2Modal() {
     let title = document.querySelector(".project-modal-title");
-    title.innerHTML = "Project 2";
+    title.innerHTML = "Sherlock API";
 
     let description = document.querySelector(".modal-description");
-    description.innerHTML = "This is a description of Project 2";
+    description.innerHTML = "The Sherlock API scrapes and parses public crime data from local authorities in Knoxville, TN to provide real-time crime reports, support self-reporting and verification, use anonymous crime-based usernames for community based posts, and can assess threat risks for specific locations.";
 
     let tech = document.querySelector(".modal-tech");
-    tech.innerHTML = "This is the tech stack used for Project 2";
+    tech.innerHTML = "Sherlock was crafted utilizing Node.js for backend functionality, SQLite3 for robust database management, and AWS Cognito for secure user authentication and verification.";
 
     let goals = document.getElementById("modal-goals");
-    goals.children[0].innerHTML = "Proj 2 Goal 1";
-    goals.children[1].innerHTML = "Proj 2 Goal 2";
-    goals.children[2].innerHTML = "Proj 2 Goal 3";
-    goals.children[3].innerHTML = "Proj 2 Goal 4";
+    goals.children[0].innerHTML = "Enable community safety awareness through real-time crime data.";
+    goals.children[1].innerHTML = "Allow for user reporting and verification of crime.";
+    goals.children[2].innerHTML = "Facilitate community driven interaction regarding local incidents.";
+    goals.children[3].innerHTML = "Enable risk assessment for specific locations.";
 
     let challenges = document.getElementById("modal-challenges");
-    challenges.children[0].innerHTML = "Proj 2 Challenge 1";
-    challenges.children[1].children[0].innerHTML = "Proj 2 more info 1";
-    challenges.children[2].innerHTML = "Proj 2 Challenge 2";
-    challenges.children[3].innerHTML = "Proj 2 Challenge 3";
+    challenges.children[0].innerHTML = "Parsing inconsistent data from local authorities.";
+    // challenges.children[1].children[0].innerHTML = "Proj 2 more info 1";
+    challenges.children[2].innerHTML = "Downloading from sources that required authorization tokens.";
+    challenges.children[3].innerHTML = "Limiting Google Maps API calls to stay within free tier.";
 }
 
 proj2Open.onclick = function() {
@@ -142,81 +142,81 @@ proj2TitleOpen.onclick = function() {
 
 // ------------------------------------- Malloc/ Project 3 Modal
 
-let proj3Open = document.getElementById("proj3-open");
-let proj3TitleOpen = document.getElementById("proj3-title");
+// let proj3Open = document.getElementById("proj3-open");
+// let proj3TitleOpen = document.getElementById("proj3-title");
 
-function populateProj3Modal() {
-    let title = document.querySelector(".project-modal-title");
+// function populateProj3Modal() {
+//     let title = document.querySelector(".project-modal-title");
 
-    title.innerHTML = "Project 3";
+//     title.innerHTML = "Project 3";
 
-    let description = document.querySelector(".modal-description");
-    description.innerHTML = "This is a description of Project 3";
+//     let description = document.querySelector(".modal-description");
+//     description.innerHTML = "This is a description of Project 3";
 
-    let tech = document.querySelector(".modal-tech");
-    tech.innerHTML = "This is the tech stack used for Project 3";
+//     let tech = document.querySelector(".modal-tech");
+//     tech.innerHTML = "This is the tech stack used for Project 3";
 
-    let goals = document.getElementById("modal-goals");
-    goals.children[0].innerHTML = "Proj 3 Goal 1";
-    goals.children[1].innerHTML = "Proj 3 Goal 2";
-    goals.children[2].innerHTML = "Proj 3 Goal 3";
-    goals.children[3].innerHTML = "Proj 3 Goal 4";
+//     let goals = document.getElementById("modal-goals");
+//     goals.children[0].innerHTML = "Proj 3 Goal 1";
+//     goals.children[1].innerHTML = "Proj 3 Goal 2";
+//     goals.children[2].innerHTML = "Proj 3 Goal 3";
+//     goals.children[3].innerHTML = "Proj 3 Goal 4";
 
-    let challenges = document.getElementById("modal-challenges");
-    challenges.children[0].innerHTML = "Proj 3 Challenge 1";
-    challenges.children[1].children[0].innerHTML = "Proj 3 more info 1";
-    challenges.children[2].innerHTML = "Proj 3 Challenge 2";
-    challenges.children[3].innerHTML = "Proj 3 Challenge 3";
+//     let challenges = document.getElementById("modal-challenges");
+//     challenges.children[0].innerHTML = "Proj 3 Challenge 1";
+//     challenges.children[1].children[0].innerHTML = "Proj 3 more info 1";
+//     challenges.children[2].innerHTML = "Proj 3 Challenge 2";
+//     challenges.children[3].innerHTML = "Proj 3 Challenge 3";
 
-}
+// }
 
-proj3Open.onclick = function() {
-    populateProj3Modal();
-    modal.style.display = "block";
-}
-proj3TitleOpen.onclick = function() {
-    populateProj3Modal();
-    modal.style.display = "block";
-}
+// proj3Open.onclick = function() {
+//     populateProj3Modal();
+//     modal.style.display = "block";
+// }
+// proj3TitleOpen.onclick = function() {
+//     populateProj3Modal();
+//     modal.style.display = "block";
+// }
 
-// ------------------------------------- Unzip/ Project 3 Modal
+// // ------------------------------------- Unzip/ Project 4 Modal
 
-let proj4Open = document.getElementById("proj4-open");
-let proj4TitleOpen = document.getElementById("proj4-title");
+// let proj4Open = document.getElementById("proj4-open");
+// let proj4TitleOpen = document.getElementById("proj4-title");
 
-function populateProj4Modal() {
-    let title = document.querySelector(".project-modal-title");
+// function populateProj4Modal() {
+//     let title = document.querySelector(".project-modal-title");
 
-    title.innerHTML = "Project 4";
+//     title.innerHTML = "Project 4";
 
-    let description = document.querySelector(".modal-description");
-    description.innerHTML = "This is a description of Project 4";
+//     let description = document.querySelector(".modal-description");
+//     description.innerHTML = "This is a description of Project 4";
 
-    let tech = document.querySelector(".modal-tech");
-    tech.innerHTML = "This is the tech stack used for Project 4";
+//     let tech = document.querySelector(".modal-tech");
+//     tech.innerHTML = "This is the tech stack used for Project 4";
 
-    let goals = document.getElementById("modal-goals");
-    goals.children[0].innerHTML = "Proj 4 Goal 1";
-    goals.children[1].innerHTML = "Proj 4 Goal 2";
-    goals.children[2].innerHTML = "Proj 4 Goal 3";
-    goals.children[3].innerHTML = "Proj 4 Goal 4";
+//     let goals = document.getElementById("modal-goals");
+//     goals.children[0].innerHTML = "Proj 4 Goal 1";
+//     goals.children[1].innerHTML = "Proj 4 Goal 2";
+//     goals.children[2].innerHTML = "Proj 4 Goal 3";
+//     goals.children[3].innerHTML = "Proj 4 Goal 4";
 
-    let challenges = document.getElementById("modal-challenges");
-    challenges.children[0].innerHTML = "Proj 4 Challenge 1";
-    challenges.children[1].children[0].innerHTML = "Proj 4 more info 1";
-    challenges.children[2].innerHTML = "Proj 4 Challenge 2";
-    challenges.children[3].innerHTML = "Proj 4 Challenge 3";
+//     let challenges = document.getElementById("modal-challenges");
+//     challenges.children[0].innerHTML = "Proj 4 Challenge 1";
+//     challenges.children[1].children[0].innerHTML = "Proj 4 more info 1";
+//     challenges.children[2].innerHTML = "Proj 4 Challenge 2";
+//     challenges.children[3].innerHTML = "Proj 4 Challenge 3";
 
-}
+// }
 
-proj4Open.onclick = function() {
-    populateProj4Modal();
-    modal.style.display = "block";
-}
-proj4TitleOpen.onclick = function() {
-    populateProj4Modal();
-    modal.style.display = "block";
-}
+// proj4Open.onclick = function() {
+//     populateProj4Modal();
+//     modal.style.display = "block";
+// }
+// proj4TitleOpen.onclick = function() {
+//     populateProj4Modal();
+//     modal.style.display = "block";
+// }
 
 // -----------------------------------------------------------------------------------
 // General Modal Functionality
